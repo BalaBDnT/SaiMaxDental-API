676a541de5e0bdff391e495b4ff3858454a5d569
BalaBDnT <122449250+BalaBDnT@users.noreply.github.com>
2026-06-03 16:08:19 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,3 +4,12 @@ export const instance = new Razorpay({
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZORPAY_APT_SECRET,
 });
+
+// initilize razorpay
+export const createOrder = async (amount) => {
+  return await instance.orders.create({
+    amount: amount * 100,
+    currency: "INR",
+    receipt: "order_rcptid_11",
+  });
+};
